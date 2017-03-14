151e41f06c271d91bb5ebeb15ff92ab1b5dc2dfd
Pedro Diaz <pds@injenia.cl>
2017-03-14 06:33:02 -0300

    New Folder Structure

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
-var User = require('../models/user');
+var User = require('../app/models/user');
 
 // Register
 router.get('/signup', function(req, res){
